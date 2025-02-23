5696528fffb47bba57a8ccf88fb879ded9beb6aa
Gan Liu <119108256+gumlau@users.noreply.github.com>
2025-02-23 13:11:48 +0800

    Initial commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,36 @@
+import { getEvents } from '@/data'
+import '@/styles/tailwind.css'
+import type { Metadata } from 'next'
+import type React from 'react'
+import { ApplicationLayout } from './application-layout'
+import { Analytics } from '@vercel/analytics/react'
+
+export const metadata: Metadata = {
+  title: {
+    template: '%s - Draft',
+    default: 'Draft',
+  },
+  description: '',
+  icons: false
+}
+
+export default async function RootLayout({ children }: { children: React.ReactNode }) {
+  let events = await getEvents()
+
+  return (
+    <html
+      lang="en"
+      className="text-zinc-950 antialiased lg:bg-zinc-100 dark:bg-zinc-900 dark:text-white dark:lg:bg-zinc-950"
+    >
+      <head>
+        <link rel="preconnect" href="https://rsms.me/" />
+        <link rel="stylesheet" href="https://rsms.me/inter/inter.css" />
+        <link rel="icon" href="data:," />
+      </head>
+      <body>
+        <ApplicationLayout events={events}>{children}</ApplicationLayout>
+        <Analytics />
+      </body>
+    </html>
+  )
+}
