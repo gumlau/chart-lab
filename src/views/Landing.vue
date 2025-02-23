ff0f4ee0fa3ccedb2776723fae03c2b699134ef6
GanLiuuuu <119108256+GanLiuuuu@users.noreply.github.com>
2025-02-23 14:12:17 +0800

    fix: route

--- src/views/Landing.vue
+++ src/views/Landing.vue
@@ -60,7 +60,7 @@
               <h1 class="text-balance text-5xl font-semibold tracking-tight text-gray-900 sm:text-7xl">{{ heroTitle }}</h1>
               <p class="mt-8 text-pretty text-lg font-medium text-gray-500 sm:text-xl/8">{{ heroDescription }}</p>
               <div class="mt-10 flex items-center justify-center gap-x-6">
-                <router-link to="/templates" class="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Browse Templates</router-link>
+                <router-link to="/" class="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Browse Templates</router-link>
                 <a href="#features" class="text-sm/6 font-semibold text-gray-900">Learn more <span aria-hidden="true">→</span></a>
               </div>
             </div>
